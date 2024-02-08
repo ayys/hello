8b1432734b7e236df8cfd73c7ab9929243cbfffe
Ayush Jha <ayush@wasmer.io>
2024-02-08 18:06:26 +0545

    embed message in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
     // read message.txt into the program
-    let message = std::fs::read_to_string("src/message.txt").unwrap();
+    let message = "Hello from Wasmer ❤️!
+===
+
+This message shows that your installation appears to be working correctly.
+
+To try something more fun, you can run cowsay with:
+ $ echo Moo | wasmer run cowsay
+
+Publish packages, run apps, and more with a free Wasmer account:
+ https://wasmer.io/
+
+For more examples and ideas, visit:
+ https://wasmer.io/templates/";
     println!("{}", message);
 }
